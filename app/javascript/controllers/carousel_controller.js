f88b5a9c8d55dfecff93a97917cf4da3199d49d1
hazrid93 <hazrid93@users.noreply.github.com>
2026-07-25 20:07:33 +0000

    Revamp StreamVault browsing experience

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -6,14 +6,37 @@ export default class extends Controller {
   connect() {
     this.scrollAmount = 300
     this.updateTimer = null
+    this.handleKeydown = this.handleKeydown.bind(this)
+    this.scheduleButtonUpdate = this.scheduleButtonUpdate.bind(this)
+    this.trackTarget.addEventListener("keydown", this.handleKeydown)
+    this.trackTarget.addEventListener("scroll", this.scheduleButtonUpdate, { passive: true })
+    window.addEventListener("resize", this.scheduleButtonUpdate)
     this.updateButtons()
   }
 
   disconnect() {
+    this.trackTarget.removeEventListener("keydown", this.handleKeydown)
+    this.trackTarget.removeEventListener("scroll", this.scheduleButtonUpdate)
+    window.removeEventListener("resize", this.scheduleButtonUpdate)
     if (this.updateTimer) {
       clearTimeout(this.updateTimer)
       this.updateTimer = null
     }
+  }
+
+  handleKeydown(event) {
+    if (event.key === "ArrowLeft") {
+      event.preventDefault()
+      this.prev()
+    } else if (event.key === "ArrowRight") {
+      event.preventDefault()
+      this.next()
+    }
+  }
+
+  scheduleButtonUpdate() {
+    if (this.updateTimer) clearTimeout(this.updateTimer)
+    this.updateTimer = setTimeout(() => this.updateButtons(), 80)
   }
 
   prev() {
